64dc087802e659d20bff5323786dd110a75a2651
bskkachu08-maker <bskkachu08@icloud.com>
2025-11-01 08:35:26 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -94,11 +94,6 @@ app.get("/photos", (_req, res) => {
   res.json(ordered);
 });
 
-// ルートアクセス時に index.html を返す
-app.get("/", (req, res) => {
- res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`+ILLuSio running at http://localhost:${PORT}`);
 });
